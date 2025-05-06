f299382ec822f8d513fe688c10661034af1126ea
nextgen-secondary <nextgensecondary25@gmail.com>
2025-05-06 12:29:11 +0530

    redesigned the Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,27 @@ import { FaClipboardList } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div id="speciality">
-        <AppointmentHandler />
+      
+      <div className="container mx-auto px-4 py-8">
+        <div id="speciality" className="mb-8">
+          <AppointmentHandler />
+        </div>
+
+        <div className="flex justify-center mb-8">
+          <NavLink 
+            to="/visit-memo" 
+            className="flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors shadow-md"
+          >
+            <FaClipboardList className="text-xl" />
+            <span className="font-medium">View Visit Memos</span>
+          </NavLink>
+        </div>
       </div>
-      <NavLink to="/visit-memo" className="nav-link">
-        <FaClipboardList className="mr-2" />
-        View Visit Memos
-      </NavLink>
-      <Banner />
+      <div className="mt-auto">
+        <Banner />
+      </div>
     </div>
   );
 };
